2c009898f8fd9a54722ffcf2726968efa5cf3945
yoshiki.ohata <yoshiki.ohata@rakus.co.jp>
2017-06-14 13:59:38 +0900

    Carにmonthを追加

--- src/main/java/com/example/demo/Car.java
+++ src/main/java/com/example/demo/Car.java
@@ -3,6 +3,14 @@ package com.example.demo;
 public class Car {
 	private int speed;
 	private String color;
+	private int month;
+	
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
 	public int getSpeed() {
 		return speed;
 	}
